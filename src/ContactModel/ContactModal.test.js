a62bfe122e11ebde4f86ae52049f31c9f819c76b
codekesh <201117@iiitt.ac.in>
2023-04-25 23:09:50 +0530

    same test case as previous

--- src/ContactModel/ContactModal.test.js
+++ src/ContactModel/ContactModal.test.js
@@ -19,3 +19,17 @@ test('Initializes empty form', async () => {
     expect(emailInput).toHaveValue('')
     expect(submitButton).toBeDisabled('')
 });
+
+test('Disable submit button until form is valid', () => {
+    render(<ContactModal />);
+    const nameInput = screen.queryByPlaceholderText('Name');
+    const phoneInput = screen.queryByPlaceholderText('Phone Number');
+    const emailInput = screen.queryByPlaceholderText('Email Address');
+    const submitButton = screen.getByText('Submit');
+
+    fireEvent.change(nameInput, { target: { value: 'Codekesh' } })
+    fireEvent.change(phoneInput, { target: { value: '7733041963' } })
+    fireEvent.change(emailInput, { target: { value: '201117@iiitt.ac.in' } })
+
+    expect(submitButton).not.toBeDisabled()
+});
