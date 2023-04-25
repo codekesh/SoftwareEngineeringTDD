fc14ccfe7c7a6a30bd86a3fc8b6d6bacd9fa0f38
codekesh <201117@iiitt.ac.in>
2023-04-25 23:06:07 +0530

    Test02 disable submit button until form is valid

--- src/ContactModel/ContactModal.test.js
+++ src/ContactModel/ContactModal.test.js
@@ -5,7 +5,6 @@ afterEach(cleanup)
 
 test('Initializes empty form', async () => {
     render(<ContactModal />);
-    
     const nameInput = screen.queryByPlaceholderText('Name');
     const phoneInput = screen.queryByPlaceholderText('Phone Number');
     const emailInput = screen.queryByPlaceholderText('Email Address');
@@ -19,4 +18,18 @@ test('Initializes empty form', async () => {
     expect(phoneInput).toHaveValue('')
     expect(emailInput).toHaveValue('')
     expect(submitButton).toBeDisabled('')
+});
+
+test('Disable submit button until form is valid', () => {
+    render(<ContactModal />);
+    const nameInput = screen.queryByPlaceholderText('Name');
+    const phoneInput = screen.queryByPlaceholderText('Phone Number');
+    const emailInput = screen.queryByPlaceholderText('Email Address');
+    const submitButton = screen.getByText('Submit');
+
+    fireEvent.change(nameInput, { target: { value: 'Codekesh' } })
+    fireEvent.change(phoneInput, { target: { value: '7733041963' } })
+    fireEvent.change(emailInput, { target: { value: '201117@iiitt.ac.in' } })
+
+    expect(submitButton).not.toBeDisabled()
 });
