9683a59e6bdb01da81e06139ee194701c787756d
sarthak503 <sarthak.kumar503@gmail.com>
2023-04-27 01:54:00 +0530

    test - edit button is clicked

--- src/ContactList/ContactList.test.js
+++ src/ContactList/ContactList.test.js
@@ -4,14 +4,14 @@ import { ContactList } from './';
 test('Renders list of contacts', () => {
   const contacts = [
     {
-      name: 'Joe',
-      email: 'test123@gmail.com',
-      phone: '987-654-3210',
+      name: 'Sarthak',
+      email: 'sarthak503@gmail.com',
+      phone: '947-354-3210',
     },
     {
-      name: 'Bob',
-      email: 'test321@gmail.com',
-      phone: '123-456-7890',
+      name: 'John',
+      email: 'Johndoe@gmail.com',
+      phone: '903-436-7890',
     },
   ];
 
@@ -20,11 +20,30 @@ test('Renders list of contacts', () => {
   const joeRow = screen.getByTestId('contact-0');
   const bobRow = screen.getByTestId('contact-1');
 
-  expect(joeRow).toHaveTextContent('Joe');
-  expect(joeRow).toHaveTextContent('test123@gmail.com');
-  expect(joeRow).toHaveTextContent('987-654-3210');
+  expect(joeRow).toHaveTextContent('Sarthak');
+  expect(joeRow).toHaveTextContent('sarthak503@gmail.com');
+  expect(joeRow).toHaveTextContent('947-354-3210');
 
-  expect(bobRow).toHaveTextContent('Bob');
-  expect(bobRow).toHaveTextContent('test321@gmail.com');
-  expect(bobRow).toHaveTextContent('123-456-7890');
+  expect(bobRow).toHaveTextContent('John');
+  expect(bobRow).toHaveTextContent('Johndoe@gmail.com');
+  expect(bobRow).toHaveTextContent('903-436-7890');
+});
+test('Calls the edit function when edit button is clicked', () => {
+  const contacts = [
+    {
+      name: 'Sarthak',
+      email: 'sarthak503@gmail.com',
+      phone: '947-354-3210',
+    },
+  ];
+
+  const editFn = jest.fn();
+
+  render(<ContactList contacts={contacts} onEditClick={editFn} />);
+
+  const editBtnJoe = screen.getByTestId('edit-btn-0');
+
+  fireEvent.click(editBtnJoe);
+
+  expect(editFn).toHaveBeenCalledWith(0);
 });
