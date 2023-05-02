126559eabf5c10c507e3cfd1cd3a0d4a2a927cf6
codekesh <201117@iiitt.ac.in>
2023-05-02 13:31:05 +0530

    contacts should be added not overwritten

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,30 @@
 import { ContactModal } from "ContactModel";
 import styles from './styles.module.css'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const App = () => {
+  const [contacts, setContacts] = useState();
   const [addingContact, setAddingContact] = useState(false)
+  
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+
+    if (!storedContacts) {
+      localStorage.setItem('contacts', JSON.stringify([]));
+      setContacts([]);
+    } else {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
+
   return (
     <div className={styles.main}>
       {addingContact && (
         <ContactModal cancel={() =>
           setAddingContact(false)}
           submit={c => {
-            console.log(c)
+            const newContacts=[...contacts, c]
+            localStorage.setItem('contacts', JSON.stringify(newContacts))
             setAddingContact(false)
           }} />
       )}
--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,44 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { App } from './';
 
-// const joe = {
-//   name: 'Sarthak',
-//   email: 'sarthak12@gmail.com',
-//   phone: '123-456-7890',
-// };
-// const bob = {
-//   name: 'Keshav',
-//   email: 'Keshav342@gmail.com',
-//   phone: '123-456-9999',
-// };
-
-// const addContact = c => {
-//   const addContactBtn = screen.getByTestId('add-contact-btn');
-
-//   fireEvent.click(addContactBtn);
-
-//   expect(
-//     screen.getByTestId('contact-modal-form'),
-//   ).toBeInTheDocument();
-
-//   const nameInput = screen.getByPlaceholderText('Name');
-//   const phoneInput = screen.getByPlaceholderText('Phone Number');
-//   const emailInput = screen.getByPlaceholderText('Email Address');
-//   const form = screen.getByTestId('contact-modal-form');
-
-//   fireEvent.change(nameInput, {
-//     target: { value: c.name },
-//   });
-//   fireEvent.change(phoneInput, {
-//     target: { value: c.phone },
-//   });
-//   fireEvent.change(emailInput, {
-//     target: { value: c.email },
-//   });
-
-//   fireEvent.submit(form);
-// };
-
 test('Shows contact modal when add contact button is clicked', () => {
   render(<App />);
 
